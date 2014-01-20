400383221e319c13bf9655b92c4836c550b0cf54
Brickgao <brickgao@gmail.com>
2014-01-20 23:36:47 +0800

    add problem-4

---
+++ 0004.py
@@ -0,0 +1,10 @@
+# -- codingutf8 --
+
+ret = -1
+for i in range(100, 1000):
+    for j in range(i, 1000):
+        _ = i * j
+        _s = str(_)
+        if _s == _s[::-1]:
+            ret = max(ret, i * j)
+print ret

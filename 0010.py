e51531f2fb95d7807b3912baa984c34d7f1456aa
brickgao <brickgao@gmail.com>
2014-01-25 21:33:22 +0800

    add problem-10

---
+++ 0010.py
@@ -0,0 +1,21 @@
+# -*- coding:utf8 -*-
+
+_is = [True for x in range(2 * 10 ** 6)]
+_is[0], _is[1] = False, False
+_p = [2]
+_e = int((2 * 10 ** 6) ** 0.5) + 1
+for i in range(4, 2 * 10 ** 6, 2):
+    _is[i] = False
+for i in range(3, _e, 2):
+    if _is[i]:
+        _p.append(i)
+        for j in range(i ** 2, 2 * 10 ** 6, i * 2):
+            _is[j] = False
+for i in range(_e | 1, 2 * 10 ** 6, 2):
+    if _is[i]:
+        _p.append(i)
+ret = 0
+for _ in _p:
+    ret += _
+print ret
+    

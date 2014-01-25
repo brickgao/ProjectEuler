57d8cada8e4e89624f4d7e51720e12dcb7c16d1b
brickgao <brickgao@gmail.com>
2014-01-25 20:55:09 +0800

    add problem-9

---
+++ 0009.py
@@ -0,0 +1,13 @@
+# -*- coding:utf8 -*-
+
+def go():
+    for i in range(1, 1000):
+        for j in range(1, 1000 - i):
+            k = 1000 - i - j
+            _ = [i, j, k]
+            _.sort()
+            if _[0] ** 2 + _[1] ** 2 == _[2] ** 2:
+                print _[0] * _[1] * _[2]
+                return
+
+go()

c81bc9008fb0352ed6d08cd407d85a762f86d7b7
brickgao <brickgao@gmail.com>
2014-01-31 13:49:14 +0800

    add problem-26

---
+++ 0026.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from decimal import *
+
+ret1, ret2 = 0, 0
+getcontext().prec = 3000
+for i in range(2, 1000):
+    _s = str(Decimal(1) / Decimal(i))
+    if len(_s) < 3002:       continue
+    _s = _s[::-1][1:]
+    for j in range(1, i):
+        if _s[:j] == _s[j:2 * j]:
+            if j > ret2:
+                ret1, ret2 = i, j
+            break
+print ret1

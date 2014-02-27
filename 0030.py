76124a23df07343f75ae6f449c0839b90066d7ec
brickgao <brickgao@gmail.com>
2014-02-27 13:34:34 +0800

    add problem-30

---
+++ 0030.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+_sum = 0
+for i in range(10, 999999):
+    _s = str(i)
+    _tmp = 0
+    for _ in _s:
+        _tmp += int(_) ** 5
+    if _tmp == i:
+        _sum += i
+print _sum
+    

75b89ca015adc9fa30fb0fd8222822620c0580b7
brickgao <brickgao@gmail.com>
2016-01-01 14:34:35 +0800

    fix 0058

--- src/0058.py
+++ src/0058.py
@@ -1,14 +1,44 @@
 # -*- coding: utf-8 -*-
 # !/bin/env python2
 
+import random
 from math import sqrt
 
 
 def cnt_prime(num):
-    for i in range(2, int(sqrt(num)) + 1):
-        if num % i == 0:
-            return 0
-    return 0 if num == 1 else 1
+    if is_probably_prime(num):
+        for i in range(2, int(sqrt(num)) + 1):
+            if num % i == 0:
+                return 0
+        return 0 if num == 1 else 1
+    else:
+        return 0
+
+
+def is_probably_prime(n, k=20):
+    if n <= 3:
+        return [False, False, True, True][n]
+    else:
+        s, d, x = 0, n - 1, 0
+        while d % 2 == 0:
+            s, d = s + 1, d >> 1
+        for i in range(k):
+            a = random.randrange(2, n)
+            x = pow(a, d, n)
+            if x == 1:
+                continue
+            tag = False
+            for j in range(s - 1):
+                if x == n - 1:
+                    tag = True
+                    break
+                x = x * x % n
+            if tag:
+                continue
+            else:
+                if x != n - 1:
+                    return False
+        return True
 
 
 def solve():

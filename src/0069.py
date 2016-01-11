10a4b11fb263458344bda7052d5ecda0431a465c
brickgao <brickgao@gmail.com>
2016-01-11 11:34:50 +0800

    add 0069

---
+++ src/0069.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# !/bin/env python2
+
+
+def solve():
+    primes, result, i = [2], 2, 3
+    while True:
+        tag = True
+        for prime in primes:
+            if i % prime == 0:
+                tag = False
+                break
+        if tag:
+            primes.append(i)
+            if result * i > 1000000:
+                return result
+            else:
+                result *= i
+        i += 1
+    return -1
+
+
+if __name__ == "__main__":
+    print solve()

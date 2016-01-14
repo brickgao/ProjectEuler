555637e61829e4d6c7d30cd21e20f50f43b844a9
brickgao <brickgao@gmail.com>
2016-01-14 12:41:13 +0800

    add 0072

---
+++ src/0072.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# !/bin/env python2
+
+
+def solve():
+    phi = [i for i in range(1000001)]
+    for i in range(2, 1000001):
+        if phi[i] == i:
+            for k in range(i, 1000001, i):
+                phi[k] -= phi[k] // i
+    return sum(phi) - 1
+
+
+if __name__ == "__main__":
+    print solve()

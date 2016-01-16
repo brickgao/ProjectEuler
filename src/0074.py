eead8fbb44515479795f049d0e16b9afc917c3ef
brickgao <brickgao@gmail.com>
2016-01-16 12:44:13 +0800

    add 0074

---
+++ src/0074.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# !/bin/env python2
+
+fac_list = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+
+
+def get_nxt(now):
+    ret = 0
+    while now > 0:
+        ret += fac_list[now % 10]
+        now = now // 10
+    return ret
+
+
+def solve():
+    result, memo = 0, dict()
+    for i in range(1000001):
+        now, vis = i, set()
+        while now not in vis and now not in memo:
+            vis.add(now)
+            now = get_nxt(now)
+        memo[i] = memo[now] + len(vis) if now in memo else len(vis)
+        if memo[i] == 60:
+            result += 1
+    return result
+
+
+if __name__ == "__main__":
+    print solve()

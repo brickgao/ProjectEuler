01e5c91274f33f4cbe3cb6794f06e2471491b3d0
brickgao <brickgao@gmail.com>
2016-01-17 12:30:16 +0800

    add 0075

---
+++ src/0075.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# !/bin/env python2
+
+from math import sqrt
+from fractions import gcd
+
+
+def solve():
+    rec = [0 for i in range(1500000)]
+    upper = int(sqrt(1500000 // 2))
+    for m in range(2, upper):
+        for n in range(1, m):
+            if (m + n) % 2 == 1 and gcd(m, n) == 1:
+                a, b, c = m * m + n * n, m * m - n * n, 2 * m * n
+                tri_len = a + b + c
+                for k in range(tri_len, 1500000, tri_len):
+                    rec[k] += 1
+    return len(filter(lambda x: x == 1, rec))
+
+
+if __name__ == "__main__":
+    print solve()

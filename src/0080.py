977468ce7d906f7dd6f6214ee5134a773e34c379
brickgao <brickgao@gmail.com>
2016-01-22 19:05:31 +0800

    add 0080

---
+++ src/0080.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# !/bin/env python2
+
+from decimal import getcontext, Decimal
+
+
+def solve():
+    total, getcontext().prec = 0, 102
+    for num in range(1, 101):
+        num_sqrt = Decimal(num).sqrt()
+        if int(num_sqrt) ** 2 == num:
+            continue
+        total += sum(map(int, str(int(num_sqrt * (10 ** 100))))[:100])
+    return total
+
+
+if __name__ == "__main__":
+    print solve()
